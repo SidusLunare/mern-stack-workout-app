b0fb0249e78be98474c691d725e575ce7f75c2c5
SidusLunare <stefanpftf@gmail.com>
2026-02-02 13:23:01 +0100

    Opdracht 09 - Authentication

--- workouts/backend/server.js
+++ workouts/backend/server.js
@@ -1,36 +1,24 @@
-// server.js
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
+import mongoose from "mongoose";
 import workoutRoutes from "./src/routes/workoutRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js"; // Nieuw!
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-// CORS toestaan voor frontend
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
-
-// Middleware
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 // Routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/auth", authRoutes); // Nieuw!
 
-// Verbind met MongoDB en start server
+// Database verbinden en server starten
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Verbonden met MongoDB");
-
-    // Start server ALLEEN als database gelukt is
-    app.listen(PORT, () => {
-      console.log(`Server draait op http://localhost:${PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log("Database verbonden & server draait");
     });
   })
-  .catch((error) => {
-    console.error("Database verbinding mislukt:", error.message);
-  });
+  .catch((err) => console.error(err));
